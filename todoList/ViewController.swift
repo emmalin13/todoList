c1e3bcf5151b2b68c5893ab2d7a7176437cd9cb6
Emma Lin <emma_r_lin@hotmail.com>
2020-07-28 10:17:33 -0500

    Initial Commit

---
+++ todoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todoList
+//
+//  Created by Emma  Lin on 7/28/20.
+//  Copyright © 2020 Emma  Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
